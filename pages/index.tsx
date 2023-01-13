34b4bb90c2735907c478e8036650b9ee423462b3
bobkori <babok303@gmail.com>
2023-01-13 22:23:13 +0530

    header completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,13 +45,13 @@ function Home() {
         </div>
       </section>
 
-      <section>
+      <section className="pt-50 pb-50">
         <div className="container">
-          <h2>Why VTS</h2>
+          <h2 className="text_center heading2">Why VTS</h2>
           <div className="row">
             <div className="vts-4">
-              <div className="boxdesign1">
-                <div className="image">
+              <div className={style.boxdesign1}>
+                <div className={style.image}>
                   <img src="/svg/popular.svg" alt="" />
                 </div>
                 <h3>It s Popular</h3>
@@ -62,6 +62,130 @@ function Home() {
                 </p>
               </div>
             </div>
+
+            <div className="vts-4">
+              <div className={style.boxdesign1}>
+                <div className={style.image}>
+                  <img src="/svg/popular.svg" alt="" />
+                </div>
+                <h3>It s Popular</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an
+                </p>
+              </div>
+            </div>
+
+            <div className="vts-4">
+              <div className={style.boxdesign1}>
+                <div className={style.image}>
+                  <img src="/svg/popular.svg" alt="" />
+                </div>
+                <h3>It s Popular</h3>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className={`${style.awardsection} pt-50 pb-50`}>
+        <div className="container">
+          <h2 className="text_center heading2 text_skyblue">
+            The problems every governemt job aspirant faces
+          </h2>
+          <div className="row mt-50">
+            <div className="vts-4">
+              <div className={style.boxdesign2}>
+                <div className={style.image}>
+                  <img src="/svg/winner1.svg" alt="" />
+                </div>
+                <div className={style.text}>
+                  <p>
+                    The Questions of The most Test Series platform are
+                    irrelevant either Too Difficult or Too Easy{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="vts-4">
+              <div className={style.boxdesign2}>
+                <div className={style.image}>
+                  <img src="/svg/winner1.svg" alt="" />
+                </div>
+                <div className={style.text}>
+                  <p>
+                    The Questions of The most Test Series platform are
+                    irrelevant either Too Difficult or Too Easy{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="vts-4">
+              <div className={style.boxdesign2}>
+                <div className={style.image}>
+                  <img src="/svg/winner1.svg" alt="" />
+                </div>
+                <div className={style.text}>
+                  <p>
+                    The Questions of The most Test Series platform are
+                    irrelevant either Too Difficult or Too Easy{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="vts-4">
+              <div className={style.boxdesign2}>
+                <div className={style.image}>
+                  <img src="/svg/winner1.svg" alt="" />
+                </div>
+                <div className={style.text}>
+                  <p>
+                    The Questions of The most Test Series platform are
+                    irrelevant either Too Difficult or Too Easy{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="vts-4">
+              <div className={style.boxdesign2}>
+                <div className={style.image}>
+                  <img src="/svg/winner1.svg" alt="" />
+                </div>
+                <div className={style.text}>
+                  <p>
+                    The Questions of The most Test Series platform are
+                    irrelevant either Too Difficult or Too Easy{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="vts-4">
+              <div className={style.boxdesign2}>
+                <div className={style.image}>
+                  <img src="/svg/winner1.svg" alt="" />
+                </div>
+                <div className={style.text}>
+                  <p>
+                    The Questions of The most Test Series platform are
+                    irrelevant either Too Difficult or Too Easy{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={style.callbtn}>
+            <Button theme="secondry">Call To Action</Button>
           </div>
         </div>
       </section>
