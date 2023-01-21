bb82cfe886c1977b23573362045f6c58221461ee
simpleneeraj <teamsimpleneeraj@gmail.com>
2023-01-21 22:44:10 +0530

    changes

--- pages/api/google/success.ts
+++ pages/api/google/success.ts
@@ -4,7 +4,6 @@ import { NextApiRequest, NextApiResponse } from "next"
 const SuccessGoogleAuth = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         const data = await fetch('http://localhost:5000/auth/google/success', {
-            // credentials: 'same-origin',
 
         })
         const result = await data.json()
