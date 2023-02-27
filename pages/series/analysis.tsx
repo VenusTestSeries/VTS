cdee0dc0eee27ffc60b9ba3fa585ef429103a669
bobkori <babok303@gmail.com>
2023-02-27 18:29:17 +0530

    new chnages

---
+++ pages/series/analysis.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "styles/analysis.module.scss";
+import Image from "next/image";
+
+const Analysis = () => {
+  return <div>Analysis</div>;
+};
+
+export default Analysis;
